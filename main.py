fae4b332439b2d8bfc8ad420c25fa6c2d7b69920
TedBarthel <info@robo-studio.de>
2022-10-25 19:57:22 +0000

    The main branch contains the template for all course days.

--- main.py
+++ main.py
@@ -8,28 +8,8 @@ def sketch(p):
     def setup():
         p.createCanvas(700, 410)
         p.background(255)
-        p.rectMode(p.CENTER)
-    
-
-    def draw():
-        colorlist = ['purple','red', 'yellow', 'turquoise', 'orange']
-        p.noStroke()
-        p.fill(random.choice(colorlist))
-        p.ellipse(p.mouseX,p.mouseY,50,50)
-
-    
-    def mousePressed(self):
-        p.background(0)
-    
-
-    def keyPressed(self):
-      if p.key==" ":
-        print("Hallo")
-    
+        p.rectMode(p.CENTER)    
 
     p.setup = setup
-    p.draw = draw
-    p.mousePressed = mousePressed
-    p.keyPressed = keyPressed
       
 myp5 = window.p5.new(sketch)
