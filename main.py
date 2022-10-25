abdcfaef2c50e1d4853a2d291cbad3ad33e3ad59
TedBarthel <info@robo-studio.de>
2022-10-25 20:03:31 +0000

    Unsere erste Errungenschaft ist es einen Kreis gezeichnet zu haben.

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def sketch(p):
     def setup():
         p.createCanvas(700, 410)
         p.background(255)
-        p.rectMode(p.CENTER)    
+        p.rectMode(p.CENTER)
+
+        # An dem ersten Kurstag haben wir gelernt einen Kreis zu zeichnen.
+        p.circle(250, 205, 20)
 
     p.setup = setup
       
